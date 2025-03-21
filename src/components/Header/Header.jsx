759f21f1df8000925133e2384b273d160ef94f86
Harshvardhan Patil <patilharshvardhan4949@gmail.com>
2025-03-21 22:30:34 +0530

    new roter router defined and createing Footer FE

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,45 @@
 import React from 'react'
+import {Conatainer,Logo,LogoutBtn } from '../index.js' 
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+
+  const authStatus = useSelector((state)=> state.auth.status)
+
+  const navigate = useNavigate()
+
+  const naItems=[
+    {
+      name :'Home',
+      slug : "/",
+      active :true
+    },
+    {
+      name :'Login',
+      slug : "/login",
+      active :!authStatus
+    },
+    {
+      name :'Signup',
+      slug : "/signup",
+      active :!authStatus
+    },
+
+    {
+      name :'All Posts',
+      slug : "/all-posts",
+      active :authStatus
+    },
+    {
+      name :'Add Posts',
+      slug : "/add-posts",
+      active :authStatus
+    },
+
+  ]
+
   return (
     <div>  Header </div>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Conatainer from "./container/Conatainer"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
     Header,
-    Footer
+    Footer,
+    Conatainer,
+    Logo,
+    LogoutBtn
 }
